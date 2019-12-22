63d3265cc866cb55b42a7ce04380f66b4f4e0516
Juan Medina <jamedina@gmail.com>
2019-12-22 11:13:02 +0000

    adding FK and Indexes

--- init.sql
+++ init.sql
@@ -1,3 +1,8 @@
+DROP INDEX IF EXISTS idx_movies_title;
+DROP INDEX IF EXISTS idx_movies_genres;
+DROP INDEX IF EXISTS idx_ratings_movie_id;
+DROP INDEX IF EXISTS idx_ratings_rating;
+
 DROP TABLE IF EXISTS movies;
 CREATE TABLE movies (
   id serial primary key,
@@ -9,7 +14,7 @@ DROP TABLE IF EXISTS ratings;
 CREATE TABLE ratings (
   id serial primary key,
   user_id int not null,
-  movie_id int not null,
+  movie_id int not null REFERENCES movies(id),
   rating float not null,
   created bigint not null
 );
--- load.sql
+++ load.sql
@@ -3,3 +3,10 @@
 
 -- ratings.csv: userId,movieId,rating,timestamp
 \copy ratings (user_id, movie_id, rating, created) FROM 'ml-latest/ratings.csv' WITH (DELIMITER ',', FORMAT CSV, HEADER true, ESCAPE '"', ENCODING 'UTF-8');
+
+CREATE INDEX idx_movies_title ON movies (title);
+CREATE INDEX idx_movies_genres ON movies (genres);
+CREATE INDEX idx_ratings_movie_id ON ratings (movie_id);
+CREATE INDEX idx_ratings_rating ON ratings (rating);
+
+vacuum full;
