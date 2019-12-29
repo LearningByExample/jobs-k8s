c8323c306e56af8b9bfae5646615ea8279a588f3
Juan Medina <jamedina@gmail.com>
2019-12-29 11:30:29 +0000

    adding exit on error

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh -
 
+set -o errexit
+
 docker build . -t localhost:32000/movie-load:registry
 docker push localhost:32000/movie-load
--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh -
 
+set -o errexit
+
 microk8s.kubectl delete jobs --selector=job-group=movie-load
 
 microk8s.kubectl create -f database-load.yml
